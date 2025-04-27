8e7a34620a70a2fe3ffdeb2aed5f44497fbaeba8
Michael <200~pregnant-mutt-cape@duck.com>
2025-04-27 10:28:18 -0500

    added comments

--- stringUtils.js
+++ stringUtils.js
@@ -1,11 +1,13 @@
+
+//Make first letter capital and then returns rest
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
-
+// reverses string
 function reverse(str) {
     return str.split('').reverse().join('');
 }
-
+// checks if string contrains the substring
 function contains(str, substr){
     return str.includes(substr);
 }
